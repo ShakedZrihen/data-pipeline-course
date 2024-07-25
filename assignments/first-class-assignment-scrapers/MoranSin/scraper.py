7e74197167af4e9f476df85640d727ac6682787e
MoranSin <nurixhbh@gmail.com>
2024-07-25 12:14:01 +0300

    fix json format and done

--- assignments/first-class-assignment-scrapers/MoranSin/scraper.py
+++ assignments/first-class-assignment-scrapers/MoranSin/scraper.py
@@ -1,25 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+from datetime import timedelta, datetime
 import dateutil.parser as parser 
+import json
+import os
+
+formatted_data = {}
 
 def convert_time(x):
     conv_time = parser.parse(x)
     format = "%H:%M"
     updated_time = conv_time + timedelta(hours=3)
-    print(updated_time.strftime(format))
+    return updated_time.strftime(format)
     
 
 def scraper():
     response = requests.get("https://www.ynet.co.il/news/category/184", 'html.parser')
     html = BeautifulSoup(response.text, 'html.parser')
-    headlines = html.find_all(attrs={'class':'title'})
-    dates = html.find_all(attrs={'class':'DateDisplay'})
-    for headline in headlines:
-        print("headlines", headline.text)
-    for time in dates:
-        if time.has_attr('datetime'):
-            convert_time(time['datetime'])
+    titleRows = html.find_all(attrs={'class':'titleRow'})
+        
+    for headline in titleRows:
+        if headline.find('time').has_attr('datetime'):
+            datetime = headline.find('time')['datetime'] 
+            hour = convert_time(datetime)
+        
+        content = headline.contents[-2].text
+        formatted_data[hour] = content
+            
+scraper()   
 
-    
-scraper()
+todays_date = datetime.now().strftime('%Y-%m-%d')
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, f'{todays_date}.json')
+
+with open(file_path, 'w', encoding='utf-8') as f:
+    json.dump(formatted_data, f, ensure_ascii=False, indent=4)
