3ba8145bdd27e84cc2c1ac383ba9c529fdd0a272
Adi Levi <adile0194@gmail.com>
2024-07-25 11:06:05 +0300

    <fix> removed comments

--- assignments/first-class-assignment-scrapers/adi-git33/first-assignment.py
+++ assignments/first-class-assignment-scrapers/adi-git33/first-assignment.py
@@ -6,6 +6,8 @@ import json
 import os
 
 def scrape_ynet():
+    #   "date": [{ "hour": "news" }]
+
     news = {}
     tz = timezone("Asia/Jerusalem")
     resp = requests.get('https://www.ynet.co.il/news/category/184')
@@ -14,26 +16,19 @@ def scrape_ynet():
     for item in div_array:
         datetime_element = ''
         time = ''
+        date = ''
         if item.find('time').has_attr('datetime'):
             datetime_element = item.find('time')['datetime']
             time = datetime.datetime.fromisoformat(datetime_element).astimezone(tz).strftime("%H:%M")
-
+            date = datetime.datetime.fromisoformat(datetime_element).astimezone(tz).strftime("%d/%m/%Y")
+            
         title = item.contents[-2].text
-        news[time] = title
+        time_json = {time : title}
+        news[date] = time_json
         
     file_path = os.path.split(os.path.realpath(__file__))[0]
     file_name = file_path + '/news.json'
     with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(news, f, ensure_ascii=False)
 
-        
-    # news = {}
-    # for item in h1:
-    #     time = item.find('span').text 
-    #     description = item.contents[-1] 
-    #     news[time] = description
-        
-    # with open('news.json', 'w', encoding='utf-8') as f:
-    #     json.dump(news, f, ensure_ascii=False)
-    
 scrape_ynet()
