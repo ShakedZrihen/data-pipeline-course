2019abe576ca45f9a2718ec5d4c2d843a0a30e2b
ronyzrihen <ronyzn3@gmail.com>
2024-07-31 00:57:33 +0300

    <feat> controller done, all functionallity completed

--- assignments/warm-up/ronyz/app.py
+++ assignments/warm-up/ronyz/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, HTTPException
+from services.controller import get_breaking_news, get_news_by_time, get_news_by_date_and_time, get_news_by_date
 
 app = FastAPI()
 
@@ -13,8 +14,18 @@ async def health():
     return {"message": "Hi I am healthy"}
 
 
-@app.get("/breaking-news")
-async def breaking_news(date: str | None = None, time: str | None = None):
-    if not date and not time:
-        return {"hihi": "hohoho"}
+@app.get("/breaking-news", status_code=200)
+async def breaking_news(res: Response, date: str | None = None, time: str | None = None):
+    try:
+        if not date and not time:
+            return get_breaking_news()
+        if date and not time:
+            return get_news_by_date(date)
+        if not date and time:
+            return get_news_by_time(time)
+        return get_news_by_date_and_time(date, time)
 
+    except HTTPException as e:
+        res.status_code = e.status_code
+        res.body = e.detail
+        return {"error": e.detail}
