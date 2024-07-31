b0a574ec21e102142a7eaf57478f104c3c2e7bc8
ofekeliyahu <104149847+ofekeliyahu@users.noreply.github.com>
2024-08-01 01:15:05 +0300

    feay

--- services/db.py
+++ services/db.py
@@ -1,8 +1,8 @@
 import json
 import os
 
+
 def read_json_by_date(date_str):
-    data=[]
     directory_path = os.path.abspath("data")
     file_name = f"{date_str}.json"
     file_path = os.path.join(directory_path, file_name)
@@ -13,7 +13,8 @@ def read_json_by_date(date_str):
     else:
         return f"No data found for date: {date_str}"
 
-def read_json_by_date_and_time(date,time):
+
+def read_json_by_date_and_time(date, time):
     directory_path = os.path.abspath("data")
     file_name = f"{date}.json"
     file_path = os.path.join(directory_path, file_name)
@@ -21,7 +22,7 @@ def read_json_by_date_and_time(date,time):
         with open(file_path, encoding='utf-8') as file:
             data = json.load(file)
         if time in data:
-            return {time:data[time]}
+            return data[time]
         else:
             return f"No  data found for time: {time}"
     else:
@@ -44,6 +45,7 @@ def read_all_json():
     else:
         return "No data found"
 
+
 def read_json_by_time(time):
     directory_path = os.path.abspath("data")
     all_data_in_time = {}
@@ -54,12 +56,10 @@ def read_json_by_time(time):
             with open(file_path, encoding='utf-8') as file:
                 data = json.load(file)
                 if time in data:
-                    data_time=data[time]
-                    print(data_time)
+                    data_time = data[time]
                     all_data_in_time[file_name] = data_time
 
     if all_data_in_time:
         return all_data_in_time
     else:
         return f"No data found for date: {time}"
-
