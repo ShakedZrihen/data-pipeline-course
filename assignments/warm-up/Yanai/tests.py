83d2e4e149919433cb3320849ed5c02ea4756a0b
Yanai <yanaiyeshurun22@gmail.com>
2024-08-01 14:15:57 +0300

    Add tests

--- assignments/warm-up/Yanai/tests.py
+++ assignments/warm-up/Yanai/tests.py
@@ -9,3 +9,13 @@ def test_breaking_news():
     assert response.status_code == 200
     assert response.json() != {"message": "No breaking news found"}
 
+def test_breaking_news_date():
+    response = requests.get("http://localhost:3000/breaking-news?date=2023-07-10")
+    print(response.json())
+    assert response.status_code == 404
+    assert response.json() == {"message": "No breaking news found"}
+
+def test_breaking_news_time():
+    response = requests.get("http://localhost:3000/breaking-news?time=23:00")
+    assert response.status_code == 404 
+    assert response.json() == {"message": "No breaking news found"}
