382af5432695387bee24c138d75df60c6a8ae7a2
adiabil <adiabil50@gmail.com>
2024-08-04 15:24:11 +0300

    first-class assignment final
    update

--- assignments/first-class-assignment-scrapers/315250688/scraper.py
+++ assignments/first-class-assignment-scrapers/315250688/scraper.py
@@ -1,28 +1,30 @@
+import json
 import os
 import requests
 from bs4 import BeautifulSoup
-import json
+from dateutil import parser
 from datetime import datetime
 
-url = 'https://news.walla.co.il/breaking'
+def fetch_news(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    articles = soup.find_all('div', class_='AccordionSection')
+    news_data = {}
+    for article in articles:
+        title = article.find('div', class_='title').text.strip()
+        timestamp = article.find('time')['datetime']
+        formatted_time = parser.parse(timestamp).strftime('%H_%M_%S')
+        news_data[formatted_time] = title
+    return news_data
 
-# TODO: handle pagination for more than 1 page
+def save_news(data, file_path):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-
-breaking_news = soup.find_all('h1', class_='breaking-item-title')
-
-formatted_data = {}
-
-for news in breaking_news:
-    hour = news.find('span', class_='red-time').text
-    content = [text for text in news.contents if isinstance(text, str)]
-    formatted_data[hour] = content[-1] 
-    todays_date = datetime.now().strftime('%Y-%m-%d') 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_dir, f'{todays_date}.json')
-
-with open(file_path, 'w', encoding='utf-8') as f:
-    json.dump(formatted_data, f, ensure_ascii=False, indent=4)
+if __name__ == '__main__':
+    url = 'https://www.ynet.co.il/news/category/184'
+    news_data = fetch_news(url)
+    date_str = datetime.today().strftime('%Y_%m_%d')
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(script_directory, f'{date_str}.json')
+    save_news(news_data, full_path)
