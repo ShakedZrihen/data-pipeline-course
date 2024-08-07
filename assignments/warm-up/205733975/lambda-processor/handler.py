252a887046411c495f47625c1fb4e6ecccdd3c1e
amitmentos <amit.ronen222@gmail.com>
2024-08-07 16:44:29 +0300

    fix: udate files and fix connection

--- assignments/warm-up/205733975/lambda-processor/handler.py
+++ assignments/warm-up/205733975/lambda-processor/handler.py
@@ -2,4 +2,10 @@ import json
 
 def process_message(event, context):
     for record in event['Records']:
-        print(json.dumps(record['body']))
+        message_body = record['body']
+        print(json.dumps(message_body))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Messages processed successfully')
+    }
--- assignments/warm-up/205733975/lambda-scraper/handler.py
+++ assignments/warm-up/205733975/lambda-scraper/handler.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 import boto3
 import json
 from services.breaking_news import fetch_news_data
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -16,12 +17,12 @@ def scrape_data():
         if not news_data:
             raise HTTPException(status_code=500, detail="Failed to fetch news data")
 
-        with open('data.json', 'w') as json_file:
-            json.dump(news_data, json_file)
+        with open('data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(news_data, json_file, ensure_ascii=False, indent=4)
 
         sqs_response = sqs.send_message(
             QueueUrl=queue_url,
-            MessageBody=json.dumps(news_data)
+            MessageBody=json.dumps(news_data, ensure_ascii=False)
         )
         if sqs_response['ResponseMetadata']['HTTPStatusCode'] == 200:
             print("Data sent to SQS successfully")
@@ -33,3 +34,5 @@ def scrape_data():
     except Exception as e:
         print(f"Exception: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
+
+handler = Mangum(app)
