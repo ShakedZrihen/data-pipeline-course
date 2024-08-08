c7deff17380af79c91f278a6d7e8714cb980ceaf
amitmentos <amit.ronen222@gmail.com>
2024-08-08 12:45:34 +0300

    fix finish first assignment

--- assignments/warm-up/205733975/lambda-processor/handler.py
+++ assignments/warm-up/205733975/lambda-processor/handler.py
@@ -3,7 +3,7 @@ import json
 def process_message(event, context):
     for record in event['Records']:
         message_body = record['body']
-        print(json.dumps(message_body))
+        print(json.dumps(message_body,ensure_ascii=False))
 
     return {
         'statusCode': 200,
