a1f5821e83c765ff69cd0bba4fdc82b079a6a37f
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2024-08-15 17:42:04 +0300

    feat: added crud and scraper functions

--- assignments/first-assignment/adi-git33/lambda-scraper/handler.py
+++ assignments/first-assignment/adi-git33/lambda-scraper/handler.py
@@ -8,4 +8,4 @@ app.include_router(router)
 handler = Mangum(app)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
